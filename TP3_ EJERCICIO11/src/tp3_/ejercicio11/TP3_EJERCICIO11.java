a0a3bc99ee3d42cf63ef201760f38cbd788c01bd
nahuelcanaza62-lgtm <nahuelcanaza62@gmail.com>
2026-04-13 20:58:23 -0300

    agrego proyecto

---
+++ TP3_ EJERCICIO11/src/tp3_/ejercicio11/TP3_EJERCICIO11.java
@@ -0,0 +1,14 @@
+
+package tp3_.ejercicio11;
+
+public class TP3_EJERCICIO11 {
+
+    
+    public static void main(String[] args) {
+ 
+      for (int i = 100; i >= 0; i -= 23) {
+          System.out.println("nuemero "+i);
+      }
+    }
+    
+}
